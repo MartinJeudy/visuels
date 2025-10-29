56e4c5009b5b0cedace1ab842d579af4f3bdb3ff
MartinJeudy <114509573+MartinJeudy@users.noreply.github.com>
2025-10-29 15:45:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1318,7 +1318,7 @@ const App = () => {
             <div style={{
               position: 'absolute',
               inset: 0,
-              padding: '24px',
+              padding: '24px 24px 24px 40px',
               display: 'flex',
               flexDirection: 'column',
               justifyContent: 'space-between',
