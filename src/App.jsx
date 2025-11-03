43f42893a66b3261818b146f42ef3d7eba2b6016
MartinJeudy <114509573+MartinJeudy@users.noreply.github.com>
2025-11-03 10:16:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -636,7 +636,7 @@ const EditPanel = memo(({
             </label>
             <input
               type="range"
-              min={selectedVisual === 'affiche' || selectedVisual === 'flyer-recto' ? 100 : 60}
+              min="60"
               max="200"
               value={imageZoom}
               onChange={onImageZoomChange}
